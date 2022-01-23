bb600da2862e52ebdca924febf0955bdfb9afffc
Dinko Osrecki <dinko.osrecki@emarsys.com>
2022-01-23 10:59:31 +0100

    feat: add semantic release

---
+++ tools/package-google-cloud-sql.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+version="$1"
+
+npx replace-in-file '0.0.0-dev' "$version" ./src/lib/version.ts
+npm run bundle
+
+cd bin
+tar --transform s/-linux// -czf "google-cloud-sql-linux.tar.gz" google-cloud-sql-linux
+tar --transform s/-macos// -czf "google-cloud-sql-macos.tar.gz" google-cloud-sql-macos
+tar --transform s/-win// -czf "google-cloud-sql-win.tar.gz" google-cloud-sql-win.exe
+cd ..
---
+++ tools/publish-brew-formula.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+version="$1"
+url="https://github.com/edosrecki/google-cloud-sql-cli/releases/download/v$version/google-cloud-sql-macos-$version.tar.gz"
+checksum=$(shasum -a 256 bin/google-cloud-sql-macos.tar.gz | awk '{ print $1 }')
+
+git clone "https://${GITHUB_TOKEN}@github.com/edosrecki/homebrew-tools.git"
+cd homebrew-tools
+
+git config user.email "${GIT_EMAIL}"
+git config user.name "Dinko Osrecki"
+
+cat <<EOF > google-cloud-sql.rb
+class GoogleCloudSql < Formula
+  desc ""
+  homepage "https://github.com/edosrecki/google-cloud-sql-cli"
+  url "$url"
+  sha256 "$checksum"
+  bottle :unneeded
+  def install
+    bin.install "google-cloud-sql"
+  end
+  test do
+    system "#{bin}/google-cloud-sql", "--version"
+  end
+end
+EOF
+
+git add google-cloud-sql.rb
+git commit -m "chore: release google-cloud-sql v$version"
+git push
+
+cd ..
+rm -rf homebrew-tools
